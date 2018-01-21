078e616e8df2c54578a06ab2da9e320595fd7228
Peter H <mlhusman@earthlink.net>
2018-01-20 16:34:22 -0800

    Finished rotations and fixed lots of bugs

--- Binary Search Tree/AVLTree.cs
+++ Binary Search Tree/AVLTree.cs
@@ -17,9 +17,25 @@ namespace BinarySearchTree
 
         public new void Delete(int key)
         {
-            Node n = new Node(key);
+            Node n = Search(key);
+            Delete(n);
+        }
+        public new void Delete(Node n)
+        {
+            Node a = n.Parent;
             base.Delete(n);
-            RecalculateHeightsAndBalances(n);
+            if (n.ChildrenCount > 1)
+            {
+                Node current = n.LChild;
+                while (current.RChild != null)
+                {
+                    current = current.RChild;
+                }
+                a = current;
+            }
+            
+            
+            RecalculateHeightsAndBalances(a);
         }
 
         public new void Insert(Node n)
@@ -31,37 +47,94 @@ namespace BinarySearchTree
 
         public void RecalculateHeightsAndBalances(Node start)
         {
+            
+
+            if (start.Balance * start.Balance > 1)
+            {
+                Rebalance(start);
+            }
             if (start.Parent == null)
             {
                 return;
-            }
-             
-            if(start.Balance * start.Balance > 1)
-            {
-                Rebalance(start);
             }
             RecalculateHeightsAndBalances(start.Parent);
         }
 
         public void Rebalance(Node n)
         {
-            if(n.Balance < -1)
+            if (n.Balance < -1)
             {
-                n.LChild.Parent = n.Parent;
-                n.Parent = n.LChild;
-                Node toBeLChild = n.LChild.RChild;
-                n.LChild.RChild = n;
-                n.LChild = toBeLChild;
+                if(n.LChild.Balance > 0)
+                {
+                    LeftRotate(n.LChild);
+                    RightRotate(n);
+                    return;
+                }
+                RightRotate(n);
 
             }
-            else if(n.Balance > 1)
+            else if (n.Balance > 1)
+            {
+                if(n.RChild.Balance < 0)
+                {
+                    RightRotate(n.RChild);
+                    LeftRotate(n);
+                    return;
+                }
+                LeftRotate(n);
+            }
+        }
+
+        public void RightRotate(Node n)
+        {
+            if (n.Parent == null)
+            {
+                Root = n.LChild;
+                n.LChild.Parent = null;
+
+            }
+            else
+            {
+                n.LChild.Parent = n.Parent;
+                if (n == n.Parent.LChild)
+                {
+                    n.Parent.LChild = n.LChild;
+                }
+                else
+                {
+                    n.Parent.RChild = n.LChild;
+                }
+            }
+            n.Parent = n.LChild;
+            Node toBeLChild = n.Parent.RChild;
+            n.Parent.RChild = n;
+            n.LChild = toBeLChild;
+        }
+
+        public void LeftRotate(Node n)
+        {
+            if (n.Parent == null)
+            {
+                Root = n.RChild;
+                n.RChild.Parent = null;
+
+            }
+            else
             {
                 n.RChild.Parent = n.Parent;
-                n.Parent = n.RChild;
-                Node toBeRChild = n.RChild.LChild;
-                n.RChild.LChild = n;
-                n.RChild = toBeRChild;
+                if (n == n.Parent.LChild)
+                {
+                    n.Parent.LChild = n.RChild;
+                }
+                else
+                {
+                    n.Parent.RChild = n.RChild;
+                }
             }
+            n.Parent = n.RChild;
+            Node toBeRChild = n.Parent.LChild;
+            n.Parent.LChild = n;
+            n.RChild = toBeRChild;
         }
 
     }
--- Binary Search Tree/Program.cs
+++ Binary Search Tree/Program.cs
@@ -32,6 +32,7 @@ namespace BinarySearchTree
                 }
                 Render(tree);
 
+
             }
         }
 
